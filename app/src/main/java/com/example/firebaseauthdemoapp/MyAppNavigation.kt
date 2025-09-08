77ae097c9b24c8799557aa26267ff00687fd69af
Chidvilas17 <chidvilas32@gmail.com>
2025-09-08 10:24:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauthdemoapp/MyAppNavigation.kt
@@ -0,0 +1,57 @@
+package com.example.firebaseauthdemoapp
+
+
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.compose.NavHost
+
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+
+import com.example.firebaseauthdemoapp.pages.HomePage
+
+import com.example.firebaseauthdemoapp.pages.LoginPage
+
+import com.example.firebaseauthdemoapp.pages.SignupPage
+
+
+
+@Composable
+
+fun MyAppNavigtion(modifier: Modifier = Modifier, authViewModel: AuthViewModel){
+
+    val navController = rememberNavController()
+
+
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+
+        composable(route = "login") {
+
+            LoginPage(modifier, navController,authViewModel)
+
+        }
+
+        composable(route = "signup") {
+
+            SignupPage(modifier, navController,authViewModel)
+
+        }
+
+        composable(route = "home") {
+
+            HomePage(modifier, navController,authViewModel)
+
+        }
+
+    })
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/firebaseauthdemoapp/pages/LoginPage.kt
@@ -0,0 +1,168 @@
+package com.example.firebaseauthdemoapp.pages
+import android.R.attr.onClick
+
+import android.widget.Toast
+
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Spacer
+
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.height
+
+import androidx.compose.material3.Button
+
+import androidx.compose.material3.OutlinedTextField
+
+import androidx.compose.material3.Text
+
+import androidx.compose.material3.TextButton
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.runtime.LaunchedEffect
+
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.livedata.observeAsState
+
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
+
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.platform.LocalContext
+
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat
+
+import androidx.navigation.NavController
+
+import com.example.firebaseauthdemoapp.AuthState
+
+import com.example.firebaseauthdemoapp.AuthViewModel
+@Composable
+fun LoginPage(modifier: Modifier = Modifier,navController: NavController, authViewModel: AuthViewModel){
+    var email by remember {
+        mutableStateOf(value = "")
+    }
+    var password by remember {
+        mutableStateOf(value = "")
+    }
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Authenticated -> navController.navigate(route = "home")
+            is AuthState.Error -> Toast.makeText(context, (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT).show()
+            else -> Unit
+
+        }
+
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+
+        verticalArrangement = Arrangement.Center,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+
+        Text(text = "Login with pavaman", fontSize = 32.sp)
+        Text(text = "Login with pavaman credentials", fontSize = 12.sp)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+        OutlinedTextField(
+
+            value =email ,
+
+            onValueChange = {
+
+                email = it
+
+            } ,
+
+            label = {
+
+                Text(text = "Email")
+
+            }
+
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+
+        OutlinedTextField(
+
+            value =password ,
+
+            onValueChange = {
+
+                password = it
+
+            } ,
+
+            label = {
+
+                Text(text = "Password")
+
+            }
+
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+        Button(onClick = {
+
+            authViewModel.login(email,password)
+
+
+
+        }){
+
+            Text(text = "Login")
+
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+
+        TextButton(onClick = {
+
+            navController.navigate(route = "signup")
+
+
+
+        }) {
+
+            Text(text = "Don't have an account, Signup")
+
+        }
+
+
+
+    }
+
+
+
+}
+
